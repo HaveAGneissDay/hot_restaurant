b06a510402229634379ff732988db4b4673686db
Clayton <claytonhthompson@gmail.com>
2018-02-10 11:54:06 -0800

    minor updates to server.js including api clear function and updating

--- server.js
+++ server.js
@@ -6,16 +6,19 @@ var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+//Server
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
+
+//Data 
+var reservation_array = [];
+var waitlist_array = [];
+
+//Routes
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-
-//Store reseverations in an array of objects
-// Name, Phone Number , Email, Unique Id
-const reservation_array = [];
-const waitlist_array = [];
-
 
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "home.html"));
@@ -30,7 +33,7 @@ app.get("/reservation/view", function (req, res) {
 });
 
 app.get("/api/reservation/list", function (req,res){
-	res.json(reservation_array);
+	res.json({reservation:reservation_array, waitlist:waitlist_array});
 });
 
 app.post("/api/reservation/create", function(req,res){
@@ -41,29 +44,14 @@ app.post("/api/reservation/create", function(req,res){
 	}else{
 		waitlist_array.push(new_reservation);
 	}
-	res.json(reservation_array);
+	res.json({reservation:reservation_array, waitlist:waitlist_array});
+});
+
+app.get("/api/reservation/clear", function(req, res){
+	reservation_array = [];
+	waitlist_array = [];
+	res.json({reservation:reservation_array, waitlist:waitlist_array});
 });
 
 
-app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
-});
-
-
-
-
-
-// Waitlist if there are 5 tables then push to a new list
-
-// Routes
-
-// GET / Reservations
-// Ajax API to fill in tables api- reservations
-
-//Post / Reservations
-
-// GET / Waitlist
-//API waitlist
-
-// Clear 
 
